a3f15a82fa8ef80bb2919e7c09519578e5e8d970
Adam Barron <adambarron@me.com>
2021-01-06 23:01:21 -0800

    installed express and verified server functions and that user routes work

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
