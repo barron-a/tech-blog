643080c4c951f7c839262f5a18196a40df6c0ece
Adam Barron <adambarron@me.com>
2021-01-08 23:46:58 -0800

    created template html and css pages in public/templates. Copied the css to public/stylesheets/style.css and set up static route in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
