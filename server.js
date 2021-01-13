5a04aa92f784bf5273c8058786d34c5e57478a18
Adam Barron <adambarron@me.com>
2021-01-12 22:51:21 -0800

    added creator and created_at fields to each post header. Also added a cookie maxage of 30 minutes after which point a user will be logged out. Verified this with a 60 second test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'The most secret secret',
-    cookie: {},
+    cookie: { maxAge: 1800000 },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
